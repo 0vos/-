ab262e1b4244a6647b70b6ce32c2bac742a8990b
mark-liusi <136895645+mark-liusi@users.noreply.github.com>
2024-12-26 14:57:12 +0800

    decode和search

--- encoder_decoder.cpp
+++ encoder_decoder.cpp
@@ -1,6 +1,7 @@
 #include "blocks.cpp"
 #include <iostream>
 #include <string>
+#include "io.cpp"
 using namespace std;
 
 // 哈夫曼树类
@@ -208,10 +209,10 @@ void EncoderTable::print_huff_table(){
 void encode(const string& content, string* encode_pair){
     // 构建一个哈夫曼表实例
     EncoderTable huff = EncoderTable(content);
-    printf("table:\n");
-    huff.print_huff_table();
-    printf("tree:\n");
-    huff.print_huff();
+    //printf("table:\n");
+    //huff.print_huff_table();
+    //printf("tree:\n");
+    //huff.print_huff();
     // 初始化字符串的编码为空、表的字符串表示为空
     string codes = "";
     string table_string = "";
@@ -242,45 +243,67 @@ void encode(const string& content, string* encode_pair){
 string decode(string name){
     string content= "";
     int i;
-    while(name[i]!= '\0'){
+    while(name[i]!= '\0'){//将json编码全部录入content
         content+= name[i];
         i++;
     }
     int number_of_words= 0;
-    for(int i=0; content[i]!= '\0';i++){
-        if(content[i]== ':'){
+    for(int i=0; content[i]!= '\0'; i++){
+        if(content[i]== ':'){//以冒号为隔断符号，用来数huff的结构体数组的个数
             number_of_words++;
-            if(content[i-1] == '\"' && content[i-2] == '\"'){
-                content.insert(i-1, 1, ' ');
+            if(content[i-1] == '\"' && content[i-2] == '\"'){//处理引号的保存方式
+                if(content[i-3]== '\\'){
+                    continue;
+                }else{
+                    content.insert(i-1, 1, ' ');
+                }
+                
             }
         }
     }
-    TableBlock *huff= new TableBlock[number_of_words];
+    TableBlock *huff= new TableBlock[number_of_words];//建立相应大小的huff结构体数组
     int j= 0;
-    //cout<<content[24]<<content[25]<<content[31]<<endl;
-    for(i=19; i<content.size(); i++){
-        huff[j].key= content[i];
-        i= i+4;
+    cout<<content<<endl;
+    int position_of_first_yinhao= content.find('\"',content.find(':'));//定位在冒号后的第一个引号，准备开始进行huff数组的录入   
+    for(i=position_of_first_yinhao; i<content.size(); i++){
+        position_of_first_yinhao= i;
+        //cout<<content[position_of_first_yinhao];
+        int position_of_second_yinhao= content.find('\"', position_of_first_yinhao+1);//这些用来处理引号的特殊情况
+        int position_of_third_yinhao= content.find('\"', position_of_second_yinhao+1);//
+        if(position_of_third_yinhao- position_of_second_yinhao== 1){//如果该字符是引号
+            huff[j].key= "\"";
+            i= content.find('\"', position_of_third_yinhao+1)+ 1;
+        }else if(content[position_of_first_yinhao+ 1]== ' '){//如果该字符是空格
+            huff[j].key= " ";
+            i= content.find('\"', position_of_second_yinhao+1)+1;
+        }else{//正常情况
+            huff[j].key= content.substr(position_of_first_yinhao, position_of_second_yinhao- position_of_first_yinhao);
+            i= content.find('\"', position_of_second_yinhao+1)+1;
+        }
+        if(j== 0){//这里调了很久，不知道哪里出现了问题，huff数组的第一个值永远都是带引号的，这个操作使其正常，后面的不受影响
+            huff[j].key= huff[j].key.substr(1);
+        }
         while(content[i]=='0'||content[i]=='1'){
-            huff[j].value+= content[i];
+            huff[j].value+= content[i];//录入哈夫曼字符串的编码
             i++;
         }
-        //cout<<i<<" "<<content[i+3]<<endl;
         cout<<huff[j].key<<" "<<huff[j].value<<endl;
-        if(content[i+1]!= ','){
+        if(content[i+1]!= ','){//判断结束标志
             break;
         }
-        i+= 2;
+        i= content.find('\"', i+1);//寻找下一个引号，判断下一次开始的位置
         j++;
     }
-    i+= 22;
-    string huff_code= content.substr(i, content.find("\"", i) - i);
-    cout << "huff code\n" <<huff_code << endl;
+    i= content.find(':', i)+3;//寻找下一个冒号，确定哈夫曼编码的开始位置
+    //cout<<content;
+    string huff_code= content.substr(i, content.find("\"", i) - i);//录入哈夫曼编码
+    //cout<<huff_code<<endl;
+    cout << "huff code\n" <<huff_code<< endl;
     string judge= "";
     string origin_content= "";
-    for(size_t i=0; i<huff_code.length(); ++i){
+    for(int i=0; i<huff_code.size(); i++){
         judge+= huff_code[i];
-        for(size_t k=0; k<j; ++k){//遍历哈夫曼编码
+        for(int k=0; k<=j; k++){//遍历哈夫曼编码
             if(judge== huff[k].value){
                 origin_content+= huff[k].key;//找到匹配并将其存入origin_content
                 judge= "";//还原judge为空
@@ -288,6 +311,7 @@ string decode(string name){
             }
         }
     }
+
     return origin_content;
 }
 string handle_special_char(string original){
@@ -320,11 +344,14 @@ string handle_special_char(string original){
     //cout<<original[position+1]<<original[position+2];
     return original;
 }
-// test
+//test
 // int main(){
 //     string list[2];
-//     encode("  Hello\nworld!", list);
-//     cout << list[0] <<endl << list[1] << endl;
-//     cout<<decode(list)<<endl;
+//     encode("我爱中国,shi\"中国\",不是#%^&国", list);
+//     //cout << list[0] <<endl << list[1] << endl;
+//     string json_content= convertToJSON("我爱中国,shi\"中国\",不是#%^&国", list[0], list[1], 0);
+//     //cout<<json_content;
+//     cout<<decode(json_content)<<endl;
+//       //Hello\nworl\"d!
 //     return 0;
 // }
--- human_desire.cpp
+++ human_desire.cpp
@@ -19,9 +19,9 @@ int repeat(int i, int j, int next[], string in_test){
     }
 }
 string search(string test, string in_test){
-    int next[in_test.size()];
+    int next[in_test.size()];//通过要匹配的字符串的长度来设置next数组的大小
     for(size_t i=0;i<in_test.size();++i){
-        next[i] = 0;
+        next[i] = 0;//将其初始化为0
     }
     int i=0, j=1;
     //计算next数组
@@ -41,22 +41,23 @@ string search(string test, string in_test){
     i=0, j=0;
     int judge= 0;
     string posion= "";
+    //以下使用kmp匹配算法
     while(j<= test.size()){
-        if(i== in_test.size()){
-            judge++;
-            posion+= to_string(j-in_test.size());
-            posion+="_";
-            i= 0;
+        if(i== in_test.size()){//j去遍历test数组，i去遍历in_test数组，当i==in_test.size()时，说明完全匹配上了
+            judge++;//judge用来计数在当前test字符串中有多少个与in_test相同的字符串
+            posion+= to_string(j-in_test.size());//position用来计数in_test字符串出现在test中的哪里，将数字以字符串的形式存储起来  
+            posion+="_";//每个位置之间用_隔开
+            i= 0;//将i清空，开始下一次匹配
         }else{
-            if(test[j]== in_test[i]){
+            if(test[j]== in_test[i]){//当字符相等时，匹配下一个
                 i++;
                 j++;
-            }else{
-                if(i== 0){
+            }else{//不相等时
+                if(i== 0){//根据kmp算法，如果已经退回至in_test头部，则开始匹配test的下一个字符
                     j++;
                 }else{
-                    i= next[i-1];
-                    if(test[j]== in_test[i]){
+                    i= next[i-1];//根据next数组，跳转至相应的in_test位置进行比较
+                    if(test[j]== in_test[i]){//相等时匹配下一个
                         i++;
                     }
                     j++;
@@ -64,10 +65,10 @@ string search(string test, string in_test){
             }
         }
     }
-    posion+= to_string(judge);
-    return posion;
+    posion+= to_string(judge);//原position中的信息为“数字_……数字_，最后给它加上这里面一共有多少个位置，即多少个数字
+    return posion;//将这个包含了所有位置和位置个数的字符串返回作为搜索的结果
 }
-int count_num_of_same(string test, string in_test){
+int count_num_of_same(string test, string in_test){//该函数用来截取position最后的位置个数，将其作为数字返回(主要是用来构建数组大小)
     int j=0, k=0;
     string posion= search(test, in_test);
     while(posion[j]!= '\0'){
@@ -79,37 +80,38 @@ int count_num_of_same(string test, string in_test){
     if(k== 0){
         return 0;
     }else{
-        string num_of_posion= posion.substr(k+1);
-        return (stoi(num_of_posion));
+        string num_of_posion= posion.substr(k+1);//截取个数字符串
+        return (stoi(num_of_posion));//将其转变为数字返回
     }
 }
-void posion_of_number(int *p, string test, string in_test){
+void posion_of_number(int *p, string test, string in_test){//该函数用于将position中的位置转换为数字存储到数组p中
     string posion= search(test, in_test);
     string temp_posion= "";
     int j=0,k= 0;
     while(posion[j]!= '\0'){
-        if(posion[j]!= '_'){
+        if(posion[j]!= '_'){//以“_”为界限进行切割
             temp_posion+= posion[j];
         }else{
-            p[k]= stoi(temp_posion);
+            p[k]= stoi(temp_posion);//将字符串转化为数字进行保存
             k++;
-            temp_posion= "";
+            temp_posion= "";//还原字符串为空
         }
         j++;
     }
 }
-string get_total_part(string test,  string in_test){
-    int count= count_num_of_same(test, in_test);
+string get_total_part(string test, string in_test){//为了以实际情况相结合以及方便用户使用，会将搜索到的in_test字符串在原test字符串中的前后5个字符进行输出，方便用户知道在哪里进行修改
+    int count= count_num_of_same(test, in_test);//
     int *p= new int(count);
-    posion_of_number(p, test, in_test);
+    posion_of_number(p, test, in_test);//将位置录入到p数组中，以数字形式存储起来
     if(count== 0){
         return "";
     }else{
-        cout<<"存在"<<endl;
+        //cout<<"存在"<<endl;
     }
     int j= 0;
     string part1, part2;
     string final_all= "";
+    //以下是对test字符串中的边缘进行检测，如果in_test字符串靠近开头，可能会出现开头到in_test没有5个字符的可能，那么就是有多少输出多少，末尾也是如此
     for(j=0; j<count; j++){
         if(p[j]-5> 0){
             part1= test.substr(p[j]-5, 5);
@@ -127,7 +129,7 @@ string get_total_part(string test,  string in_test){
             }
         }    
         //cout<<j+1<<"."<<"\033[31m"<<part1<<" "<<"\033[33m"<<part2<<"\033[0m"<<endl;
-        final_all+= (to_string(j+1)+ "."+ part1 +"("+in_test+")"+part2+ "\n");
+        final_all+= (to_string(j+1)+ "."+ part1 +"("+in_test+")"+part2+ "\n");//加括号，使其更加醒目
     }
     cout<<final_all;
     return final_all;
